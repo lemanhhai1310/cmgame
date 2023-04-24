9d4dd5aa1097225cc34593ba570f03706c8c9701
hailm <hai.lemanh.1310@gmail.com>
2023-04-24 15:22:58 +0700

    update

--- lienhe.php
+++ lienhe.php
@@ -11,8 +11,40 @@
 </div>
 <div class="uk-section">
     <div class="uk-container">
-        <div class="hoatdong__boxContent uk-section-small">
+        <div class="hoatdong__boxContent uk-section-small uk-text-center">
+            <h1>Liên hệ</h1>
 
+
+            <div>Gửi email cho chúng tôi hoặc liên lạc theo số điện thoại bên dướichúng tôi luôn chào đón bạn.</div>
+            <div>Thanks!</div>
+            <div uk-grid>
+                <div>
+                    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="50" cy="50.0001" r="49" stroke="#F8F8F8" stroke-width="2"/>
+                        <mask id="mask0_118_412" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="18" y="18" width="64" height="65">
+                            <path d="M18 18.0001H82V82.0001H18V18.0001Z" fill="white"/>
+                        </mask>
+                        <g mask="url(#mask0_118_412)">
+                            <path d="M31.75 55.7501L21.35 38.6864C20.0238 36.7014 19.25 34.3164 19.25 31.7501C19.25 24.8464 24.8463 19.2501 31.75 19.2501C38.6538 19.2501 44.25 24.8464 44.25 31.7501C44.25 34.3164 43.4763 36.7014 42.15 38.6864L31.75 55.7501Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M36.75 31.7501C36.75 34.5114 34.5113 36.7501 31.75 36.7501C28.9887 36.7501 26.75 34.5114 26.75 31.7501C26.75 28.9889 28.9887 26.7501 31.75 26.7501C34.5113 26.7501 36.75 28.9889 36.75 31.7501Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M68.25 75.7501C69.6313 75.7501 70.75 76.8701 70.75 78.2501C70.75 79.6314 69.6313 80.7501 68.25 80.7501C66.8687 80.7501 65.75 79.6314 65.75 78.2501C65.75 76.8701 66.8687 75.7501 68.25 75.7501Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M68.25 75.7501L57.85 59.4364C56.5238 57.4514 55.75 55.0664 55.75 52.5001C55.75 45.5964 61.3463 40.0001 68.25 40.0001C75.1538 40.0001 80.75 45.5964 80.75 52.5001C80.75 55.0664 79.9763 57.4514 78.65 59.4364L68.25 75.7501Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M73.25 52.5001C73.25 55.2614 71.0113 57.5001 68.25 57.5001C65.4887 57.5001 63.25 55.2614 63.25 52.5001C63.25 49.7389 65.4887 47.5001 68.25 47.5001C71.0113 47.5001 73.25 49.7389 73.25 52.5001Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M34.25 58.2501C34.25 59.6314 33.1313 60.7501 31.75 60.7501C30.3687 60.7501 29.25 59.6314 29.25 58.2501C29.25 56.8701 30.3687 55.7501 31.75 55.7501C33.1313 55.7501 34.25 56.8701 34.25 58.2501Z" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M49.375 78.2501C49.375 78.9405 48.8154 79.5001 48.125 79.5001C47.4346 79.5001 46.875 78.9405 46.875 78.2501C46.875 77.5597 47.4346 77.0001 48.125 77.0001C48.8154 77.0001 49.375 77.5597 49.375 78.2501Z" fill="black"/>
+                            <path d="M34.25 58.2501H45C47.7613 58.2501 50 60.4889 50 63.2501C50 66.0114 47.7613 68.2501 45 68.2501H30C27.2388 68.2501 25 70.4889 25 73.2501C25 76.0114 27.2388 78.2501 30 78.2501H42.5" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M53.75 78.2501H65.75" stroke="#FF9431" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                    </svg>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
